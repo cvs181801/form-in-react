3b7e7ad48c7d0e6fb003a94669d9d0175bd972b0
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-23 14:57:39 -0700

    builda simple form using a react controlled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const {name,value} = event.target;
+    this.setState({
+      [name]:value
+    })
+
+  }
+  render() {
+    return (
+      <div className="App">
+      <input
+      type="text"
+      name="firstName"
+      placehoder="first name"
+      onChange={this.handleChange}
+      >
+      </input>
+      <input 
+       type="text"
+       name="lastName"
+       placehoder="last name"
+       onChange={this.handleChange}
+       ></input>
+
+       <h1>{this.state.firstName} {this.state.lastName}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
